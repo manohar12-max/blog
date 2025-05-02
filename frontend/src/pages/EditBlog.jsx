1be40304b60194b63ab7f8decdb9e1d496e6b04a
Manohar Dhumal <2898manohar@gmail.com>
2025-05-02 10:44:37 +0530

    Seventh commit clean up code

--- frontend/src/pages/EditBlog.jsx
+++ frontend/src/pages/EditBlog.jsx
@@ -1,4 +1,4 @@
-// pages/EditBlog.js
+
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useUserContext } from "../context/UserContext";
