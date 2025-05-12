3d5d7d168a85d06547685b6eb8a36f555af44a40
Manohar Dhumal <127774463+manohar12-max@users.noreply.github.com>
2025-05-12 11:37:40 +0530

    Update blogModal.js

--- backend/models/blogModal.js
+++ backend/models/blogModal.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const blogSchema = mongoose.Schema(
   {
     title: { type: String, required: true },
-    description: { type: String, required: true },
+    description: { type: String, required: true , minLength: [500,"Minimum 200 words are required."]},
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User", 
